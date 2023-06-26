5672085f4790401d3755c05b66921ae909ef0947
Toby Rea <toby.e.rea@protonmail.com>
2023-06-26 18:49:44 +0100

    Pool results into single JSON file

--- scrape.sh
+++ scrape.sh
@@ -1,26 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-mkdir -p datasets && cd datasets
+scrape_datasets() {
+    file="${tmp_dir}/${component}.json"
+    curl -s https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx?Component=${component} \
+    | hq \
+        "{ ${component}: #GridView1 > tbody > tr  | \
+            [{ \
+                years: td:nth-child(1), \
+                name: td:nth-child(2), \
+                docs: td:nth-child(3) > a | @(href), \
+                data: td:nth-child(4) > a | @(href), \
+                published: td:nth-child(5) \
+            }] \
+        }" \
+    | jq \
+    | sed '/\"\(data\|docs\)\": \"/ s/\"\//\"https\:\/\/wwwn\.cdc\.gov\//' \
+    > $file
+    count=$(jq '.[] | length' $file)
+    printf '[LOG] Discovered %d %s datasets\n' "$count" "$component"
+}
 
-COMPONENTS=("Demographics" "Dietary" "Examination" "Laboratory" "Questionnaire")
+tmp_dir=$(mktemp -d)
 
-for component in "${COMPONENTS[@]}"
-do
-file="${component}.json"
-curl -s https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx?Component=${component} \
-| hq \
-    "{ datasets: #GridView1 > tbody > tr  | \
-        [{ \
-            years: td:nth-child(1), \
-            name: td:nth-child(2), \
-            docs: td:nth-child(3) > a | @(href), \
-            data: td:nth-child(4) > a | @(href), \
-            published: td:nth-child(5) \
-        }] \
-    }" \
-| jq \
-| sed '/\"\(data\|docs\)\": \"/ s/\"\//\"https\:\/\/wwwn\.cdc\.gov\//' \
-> $file
-count=$(jq '.datasets | length' $file)
-printf '[LOG] Discovered %d %s datasets\n' "$count" "$component"
+for component in "Demographics" "Dietary" "Examination" "Laboratory" "Questionnaire"; do
+    scrape_datasets $component
 done
+
+jq -s 'add' ${tmp_dir}/*.json > Available_Datasets.json
